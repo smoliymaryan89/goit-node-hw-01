65c121c9a5a0375fff2983f71139ab17507e1430
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-09-21 20:56:55 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+import { program } from "commander";
+import * as contactService from "./contacts.js";
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contactList = await contactService.listContacts();
+      console.log(contactList);
+      break;
+
+    case "get":
+      const oneContact = await contactService.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contactService.addContact({
+        name,
+        email,
+        phone,
+      });
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const deleteContact = await contactService.deleteContact(id);
+      console.log(deleteContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+invokeAction(options);
