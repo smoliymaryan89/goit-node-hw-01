8ff71572cf51046d71f8a12c44910b65c73c49ea
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-09-21 21:14:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,34 @@ import { program } from "commander";
 import * as contactService from "./contacts.js";
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
-  switch (action) {
-    case "list":
-      const contactList = await contactService.listContacts();
-      console.table(contactList);
-      break;
+  try {
+    switch (action) {
+      case "list":
+        const contactList = await contactService.listContacts();
+        console.table(contactList);
+        break;
 
-    case "get":
-      const oneContact = await contactService.getContactById(id);
-      console.log(oneContact);
-      break;
+      case "get":
+        const oneContact = await contactService.getContactById(id);
+        console.log(oneContact);
+        break;
 
-    case "add":
-      const newContact = await contactService.addContact(name, email, phone);
-      console.log(newContact);
-      break;
+      case "add":
+        const newContact = await contactService.addContact(name, email, phone);
+        console.log(newContact);
+        break;
 
-    case "remove":
-      const deleteContact = await contactService.removeContact(id);
-      console.log(deleteContact);
-      break;
+      case "remove":
+        const deleteContact = await contactService.removeContact(id);
+        console.log(deleteContact);
+        break;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  } catch (error) {
+    console.log(error.message);
+    throw error;
   }
 };
 
